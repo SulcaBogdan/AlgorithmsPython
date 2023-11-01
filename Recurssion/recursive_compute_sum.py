51caf8f62919a21393034ab68807c5f334613401
SulcaBogdan <Stefanbogdansulca@gmail.com>
2023-11-01 13:12:15 +0200

    recursion sum

--- Recurssion/recursive_compute_sum.py
+++ Recurssion/recursive_compute_sum.py
@@ -1,3 +1,4 @@
+
 def recursive_compute_sum(lista):
     if len(lista) == 0:
         return 0
@@ -7,5 +8,5 @@ def recursive_compute_sum(lista):
         suma = first + recursive_compute_sum(rest)
         return suma
 
-
+"""Apelam recursiv functa compute_sum pentru a efectua calcularea sumei elementelor din lista"""
 print(recursive_compute_sum([1,2,3,4,5]))
